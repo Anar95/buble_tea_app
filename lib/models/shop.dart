f97e089502654e06c80da5cb77dd51c9407821a1
Anar95 <anarabbas458@gmail.com>
2023-04-10 01:59:16 +0300

    add ımage

--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -5,10 +5,18 @@ class BubleTeaShop extends ChangeNotifier {
   //list for drinks for sale
   final List<Drink> _shop = [
     //pearl milk tea
-    Drink(
-        name: ' Milk Tea', price: '5.00', imagePath: 'lib/images/image_1.png'),
-   Drink( 
-    name: ' Lemon Tea', price: '5.00', imagePath: 'lib/images/image_2.png'),
+        Drink(
+        name: ' Milk Bubble Tea', price: '5.00', imagePath: 'lib/images/image_1.png'),
+        Drink( 
+        name: ' Lemon Bubble Tea', price: '4.00', imagePath: 'lib/images/image_2.png'),
+        Drink( 
+        name: ' Caramel Chai Bubble Tea', price: '7.00', imagePath: 'lib/images/image_3.png'),
+        Drink(
+        name: ' Tealive Bubble Milk Tea', price: '5,5.00', imagePath: 'lib/images/image_4.png'),
+        Drink( 
+        name: ' Tai Chi Pearl Milk Te', price: '8.00', imagePath: 'lib/images/image_5.png'),
+        Drink( 
+        name: ' Vanilla Bubble Milk Tea', price: '5,99.00', imagePath: 'lib/images/image_6.png'),
   ];
 
   //list of drinks in user cart
